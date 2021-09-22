e7153d3f3cd92fdb8e53fc99eec42a14dc344a72
Jerome Tolentino <tolent.jay@gmail.com>
2021-09-22 17:55:34 +0900

    tyepscript practice

--- sandbox.ts
+++ sandbox.ts
@@ -9,3 +9,43 @@ const circ = (diameter: number) => {
 };
 
 console.log(circ(20));
+
+// arrays
+
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+// numbers.push('shaun')
+// numbers[1] = 'shaun'
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+
+let ninja = {
+  name: 'Mario',
+  belt: 'black',
+  age: 30,
+};
+
+ninja.age = 40;
+ninja.name = 'Ryu';
+// ninja.age = '30'
+
+// ninja.skills = ['fighting', 'sneaking'] // you cannot add more properties any more once it is delcared
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+};
+
+// Typescript is a strict biatch
