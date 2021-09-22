772dcc6f637fcf96b5a78a969ff45c293a9d750b
Jerome Tolentino <tolent.jay@gmail.com>
2021-09-22 18:42:06 +0900

    added union types

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,7 @@
-var character = 'mario';
-var age = 30;
+// let character = 'mario';
+// let age = 30;
 var isBlackBelt = false;
-age = 31;
+// age = 31;
 var circ = function (diameter) {
     return diameter * Math.PI;
 };
@@ -13,10 +13,10 @@ var numbers = [10, 20, 30, 40];
 numbers.push(25);
 // numbers.push('shaun')
 // numbers[1] = 'shaun'
-var mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+// let mixed = ['ken', 4, 'chun-li', 8, 9];
+// mixed.push('ryu');
+// mixed.push(10);
+// mixed[0] = 3;
 // objects
 var ninja = {
     name: 'Mario',
@@ -32,4 +32,24 @@ ninja = {
     belt: 'orange',
     age: 40
 };
-// Typescript is a strict biatch
+// Conclusion: Typescript is a strict biatch
+// Explicit Types
+var character; //initialize but no value yet but type is explicitly typed
+var age;
+var isLoggedIn;
+// arrays
+var ninjas = [];
+ninjas.push('shaun');
+//union types - are types that can be either in the selection
+var mixed = []; // explicitly declaring the type of array in the mixed variable
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+var uid;
+uid = '123';
+uid = 123;
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+ninjaOne = [];
+var ninjaTwo;
+ninjaTwo = { name: 'Mario', age: 20, beltColour: 'black' };
--- sandbox.ts
+++ sandbox.ts
@@ -1,8 +1,8 @@
-let character = 'mario';
-let age = 30;
+// let character = 'mario';
+// let age = 30;
 let isBlackBelt = false;
 
-age = 31;
+// age = 31;
 
 const circ = (diameter: number) => {
   return diameter * Math.PI;
@@ -22,11 +22,11 @@ numbers.push(25);
 // numbers.push('shaun')
 // numbers[1] = 'shaun'
 
-let mixed = ['ken', 4, 'chun-li', 8, 9];
+// let mixed = ['ken', 4, 'chun-li', 8, 9];
 
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+// mixed.push('ryu');
+// mixed.push(10);
+// mixed[0] = 3;
 
 // objects
 
@@ -48,4 +48,39 @@ ninja = {
   age: 40,
 };
 
-// Typescript is a strict biatch
+// Conclusion: Typescript is a strict biatch
+
+// Explicit Types
+
+let character: string; //initialize but no value yet but type is explicitly typed
+let age: number;
+let isLoggedIn: boolean;
+
+// arrays
+
+let ninjas: string[] = [];
+
+ninjas.push('shaun');
+
+//union types - are types that can be either in the selection
+let mixed: (string | number | boolean)[] = []; // explicitly declaring the type of array in the mixed variable
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+
+let uid: string | number;
+uid = '123';
+uid = 123;
+
+let ninjaOne: object;
+
+ninjaOne = { name: 'yoshi', age: 30 };
+ninjaOne = [];
+
+let ninjaTwo: {
+  name: string;
+  age: number;
+  beltColour: string;
+};
+
+ninjaTwo = { name: 'Mario', age: 20, beltColour: 'black' };
