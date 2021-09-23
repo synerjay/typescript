8036e3195212238021d8b8be0fbd5201c25761d6
Jerome Tolentino <tolent.jay@gmail.com>
2021-09-23 18:02:28 +0900

    Added interfaces

--- public/app.js
+++ public/app.js
@@ -1,4 +1,23 @@
 import { Invoice } from './classes/Invoice.js'; // the file extension must be js, and not a ts!! Because at the end of the day we are compiling to these to a JS file!
+const me = {
+    name: 'shaun',
+    //age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent ', amount);
+        return amount;
+    },
+};
+console.log(me);
+me.age = 30;
+me.speak('hello, world');
+const greetPerson = (person) => {
+    console.log('hello ', person.name);
+};
+greetPerson(me);
+// greetPerson({ name: 'Jerome'})  -- throw an error
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,37 @@
 import { Invoice } from './classes/Invoice.js'; // the file extension must be js, and not a ts!! Because at the end of the day we are compiling to these to a JS file!
 
+// interfaces
+
+interface IsPerson {
+  name: string;
+  age?: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: 'shaun',
+  //age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount);
+    return amount;
+  },
+};
+
+console.log(me);
+me.age = 30;
+me.speak('hello, world');
+
+const greetPerson = (person: IsPerson): void => {
+  console.log('hello ', person.name);
+};
+
+greetPerson(me);
+// greetPerson({ name: 'Jerome'})  -- throw an error
+
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
