dc5b7e1baa57d306b4a595c6f61eff3614ef3fdc
Jerome Tolentino <tolent.jay@gmail.com>
2021-09-23 16:38:53 +0900

    added classes to the app ts file

--- public/app.js
+++ public/app.js
@@ -1,1 +1,32 @@
 "use strict";
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// invoices.push({ name: 'shaun' });
+console.log(invoices);
+// const anchor = document.querySelector('a')!;
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    // the event type is a built-in TS type that tells TS that e is an event variable
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
---
+++ src/app.ts
@@ -0,0 +1,44 @@
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// invoices.push({ name: 'shaun' });
+
+console.log(invoices);
+
+// const anchor = document.querySelector('a')!;
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+console.log(form.children);
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  // the event type is a built-in TS type that tells TS that e is an event variable
+  e.preventDefault();
+
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
